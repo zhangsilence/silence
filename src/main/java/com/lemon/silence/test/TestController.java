780b4cfa7ffdb7e9dcee21ab001e5c199030ea35
silence <zhangxueqi121@163.com>
2021-03-03 15:11:52 +0800

    测试功能构建

--- src/main/java/com/lemon/silence/test/TestController.java
+++ src/main/java/com/lemon/silence/test/TestController.java
@@ -1,4 +1,20 @@
 package com.lemon.silence.test;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author silence
+ */
+@Slf4j
+@RestController
+@Api(tags = "新功能测试接口")
+@RequestMapping(path = "/lemon/api/test")
 public class TestController {
+
+    @Autowired
+    private TestService testService;
 }
---
+++ src/main/java/com/lemon/silence/test/TestService.java
@@ -0,0 +1,12 @@
+package com.lemon.silence.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author silence
+ */
+@Slf4j
+@Service
+public class TestService {
+}
